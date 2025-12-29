99a12c27d026cb10a036fcf9f2b25f58c856a538
Alibaba Abdulbasit <reachalibee@gmail.com>
2025-12-29 15:29:45 +0100

    utils, exception & logger added

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-from src.ChickenDiseaseClassifier import logger
-from 
+# from ChickenDiseaseClassifier import logger
+
+from ChickenDiseaseClassifier import logger
+
+logger.info("Welcome to Chicken Disease Analyzer")
