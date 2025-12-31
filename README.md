dcdbdd98336b537515abb8740c7ffe86b67518f4
Alibaba Abdulbasit <reachalibee@gmail.com>
2025-12-31 14:38:57 +0100

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Chicken-Disease-Classifier-With-Mlflow-and-DVC
+
+
+## Workflow
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager src config
+6. Update the components\
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # from ChickenDiseaseClassifier import logger
 
 from ChickenDiseaseClassifier import logger
+from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Chicken Disease Analyzer")
+STAGE_NAME = "Data Ingestion Stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
