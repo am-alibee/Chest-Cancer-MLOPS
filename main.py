59e2ca93a51d9797d9ab14ace4ac1e6dcdb7ae83
Alibaba Abdulbasit <reachalibee@gmail.com>
2026-01-01 12:06:57 +0100

    Prepared base model added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -11,6 +12,19 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"****************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from ChickenDiseaseClassifier.constants import *
 from ChickenDiseaseClassifier.utils.common import read_yaml, create_directories
-from ChickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig)
+from ChickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAM_FILE_PATH  = Path("params.yaml")
@@ -37,3 +37,21 @@ class ConfigurationManager:
 
         return data_ingestion_config
         
+    # update config manager for stage 2
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
